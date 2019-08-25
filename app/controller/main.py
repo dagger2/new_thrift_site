f958b4a0d1064f826837f602b5885505c518731e
Bryant Lee <bl692@cornell.edu>
2019-08-25 19:35:39 -0400

    updated content

--- app/controller/main.py
+++ app/controller/main.py
@@ -10,6 +10,28 @@ def index():
     return render_template('main/index.html',
                            title='Cornell Thrift Test')
 
+@bp.route('/events/ezras_exchange')
+def ezras_exchange():
+    return render_template('main/hello.html')
+
+@bp.route('/events/mending-workshops')
+def mending_workshops():
+    return render_template('main/hello.html')
+
+@bp.route('/e-board')
+def e_board():
+    return render_template('main/e-board.html')
+
+@bp.route('/events/pop-up-shops')
+def pop_up_shops():
+    return render_template('main/hello.html')
+
 @bp.route('/hello')
 def hello():
     return render_template('main/hello.html')
+
+@bp.route('/dino-game')
+def dino_game():
+    return render_template('main/dino_game.html')
+
+# @bp.errorhandler(404)
