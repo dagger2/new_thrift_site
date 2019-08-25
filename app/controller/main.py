e3ccfeee4a03caa3c54ed23decf13251300a2f76
Bryant Lee <bl692@cornell.edu>
2019-08-25 15:13:27 -0400

    merged parallax materialize with flask

--- app/controller/main.py
+++ app/controller/main.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, app, Flask
 )
 
 bp = Blueprint('main', __name__)
@@ -8,4 +8,8 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('main/index.html',
-                           title='Flask-PWA')
+                           title='Cornell Thrift Test')
+
+@bp.route('/hello')
+def hello():
+    return render_template('main/hello.html')
