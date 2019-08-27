ebbca500a84fb55f2465b42e7f790a327447333d
Bryant Lee <bl692@cornell.edu>
2019-08-26 20:13:30 -0400

    modified nav UI

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -1,22 +1,21 @@
 $(document).ready(function(){
   $('.parallax').parallax();
   $('.sidenav').sidenav();
-
   if($(this).scrollTop() <= 0){
+    $('nav').removeClass('add-shadow');
     $('.home-logo').removeClass('home-logo-below');
   }
-
   $(document).scroll(function() {
     if($(this).scrollTop() <= 0){
       $('.home-logo').removeClass('home-logo-below');
+      $('nav').removeClass('add-shadow');
     }
     else{
       $('.home-logo').addClass('home-logo-below');
+      $('nav').addClass('add-shadow');
     }
   });
-
   $('#refresh').click(function(){
     window.location.reload(true);
   });
-
 });
