8397a0485d57bfa92a0f727b2624fd3f4d065a83
Bryant Lee <bl692@cornell.edu>
2019-08-27 14:18:19 -0400

    re-enabled SW

--- app/controller/pwa.py
+++ app/controller/pwa.py
@@ -4,14 +4,12 @@ from flask import (
 
 bp = Blueprint('pwa', __name__, url_prefix='')
 
-
 @bp.route('/manifest.json')
 def manifest():
     return send_from_directory('static', 'manifest.json')
 
-
-# @bp.route('/sw.js')
-# def service_worker():
-#     response = make_response(send_from_directory('static', 'sw.js'))
-#     response.headers['Cache-Control'] = 'no-cache'
-#     return response
+@bp.route('/sw.js')
+def service_worker():
+    response = make_response(send_from_directory('static', 'sw.js'))
+    response.headers['Cache-Control'] = 'no-cache'
+    return response
